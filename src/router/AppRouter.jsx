021539e3a32efb8d6e3dda897a935fb29be920b8
Ale Cechi <alejandrocechi@hotmail.com>
2024-06-20 06:08:56 -0300

    Rutas privadas

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,18 +1,23 @@
-import {Routes, Route, Navigate} from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import { ArgPages } from '../ciudades/pages/ArgPages'
 import { BraPages } from '../ciudades/pages/BraPages'
 import { LoginPage } from '../auth/pages/LoginPage'
 import { CiudadesRoutes } from '../ciudades/routes/CiudadesRoutes'
+import { PrivateRouter } from './PrivateRouter'
 
 export const AppRouter = () => {
   return (
     <>
-    <Routes>
+      <Routes>
 
-            <Route path='login' element = { <LoginPage/>} />
-            <Route path='/*' element ={<CiudadesRoutes/>} />
+        <Route path='login' element={<LoginPage />} />
+        {/* <Route path='/*' element={<CiudadesRoutes />} /> */}
+        <Route path='/*' element={<PrivateRouter><CiudadesRoutes /> </PrivateRouter>} />
 
-    </Routes>  
+
+
+
+      </Routes>
     </>
   )
 }
