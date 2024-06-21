965c944a9dbafbb3a889afa92df3cbfde146f0bd
Ale Cechi <alejandrocechi@hotmail.com>
2024-06-21 00:41:17 -0300

    Login como ruta publica - no para los ya logueados

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -4,14 +4,17 @@ import { BraPages } from '../ciudades/pages/BraPages'
 import { LoginPage } from '../auth/pages/LoginPage'
 import { CiudadesRoutes } from '../ciudades/routes/CiudadesRoutes'
 import { PrivateRouter } from './PrivateRouter'
+import { PublicRouter } from './PublicRouter'
 
 export const AppRouter = () => {
   return (
     <>
       <Routes>
 
-        <Route path='login' element={<LoginPage />} />
+        {/*<Route path='login' element={<LoginPage />} />*/}
         {/* <Route path='/*' element={<CiudadesRoutes />} /> */}
+        <Route path='login' element={<PublicRouter><LoginPage /> </PublicRouter>} />
+
         <Route path='/*' element={<PrivateRouter><CiudadesRoutes /> </PrivateRouter>} />
 
 
