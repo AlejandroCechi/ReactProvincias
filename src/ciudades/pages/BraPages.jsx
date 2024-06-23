da20dc07b54d1a184c918da6d1b30d7681f2c051
Ale Cechi <alejandrocechi@hotmail.com>
2024-06-23 18:58:21 -0300

    imagenes de provincias

--- src/ciudades/pages/BraPages.jsx
+++ src/ciudades/pages/BraPages.jsx
@@ -2,7 +2,7 @@ import { CiudadList } from "../components/CiudadList"
 
 export const BraPages = () => {
   return (
-    <>BraPages
+    <>
     <CiudadList country='Br' ></CiudadList>
     </>
   )
